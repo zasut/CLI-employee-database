62648326d8265814d3b381e935ee037b61dd7896
jaden <66454947+zasut@users.noreply.github.com>
2025-11-19 15:21:32 -0800

    Implements file IO

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 #include <stdbool.h>
 #include <getopt.h>
 
+#include "common.h"
+#include "file.h"
 
 
 void print_usage(char *argv[]) {
@@ -17,6 +19,8 @@ int main(int argc, char *argv[]) {
     char *filepath = NULL;
     bool newfile = false;
     int c;
+
+    int dbfd = -1;
 
     while((c = getopt(argc, argv, "nf: ")) != -1) {
         switch (c) {
@@ -40,6 +44,25 @@ int main(int argc, char *argv[]) {
 
         return 0;
     }
+
+
+    if (newfile) {
+        dbfd = create_db_file(filepath);
+        if (dbfd == STATUS_ERROR) {
+            printf("Unable to create database file\n");
+            return -1;
+        }
+    } else {
+        dbfd = open_db_file(filepath);
+        if (dbfd == STATUS_ERROR) {
+            printf("Unable to open database file\n");
+            return -1;
+        }
+    }
+
+
+
+
 
     printf("Newfile: %d\n", newfile);
     printf("Filepath: %s/n", filepath);
