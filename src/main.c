54b5f27446f6f23c36478ee136fff958b79aeb30
jaden <66454947+zasut@users.noreply.github.com>
2025-11-19 14:56:25 -0800

    implements command line utilities

---
+++ src/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <getopt.h>
+
+
+
+void print_usage(char *argv[]) {
+    printf("Usage: %s -n -f <database file>\n", argv[0]);
+    printf("\t -n - create new database file\n");
+    printf("\t -f - (required) path to database file\n");
+    return;
+}
+
+
+int main(int argc, char *argv[]) {
+
+    char *filepath = NULL;
+    bool newfile = false;
+    int c;
+
+    while((c = getopt(argc, argv, "nf: ")) != -1) {
+        switch (c) {
+            case 'n':
+                    newfile = true;
+                    break;
+            case 'f':
+                    filepath = optarg;
+                    break;
+             case '?':
+                    printf("Unknown option -%c\n", c);
+                    break;
+            default:
+                    return -1;
+        }
+    }
+
+    if (filepath == NULL) {
+        printf("Filepath is a required argument\n");
+        print_usage(argv);
+
+        return 0;
+    }
+
+    printf("Newfile: %d\n", newfile);
+    printf("Filepath: %s/n", filepath);
+
+    return 0;
+
+}
