bc696111061c5a2bb7b9991d7ea9aeb8ddf162ba
jaden <66454947+zasut@users.noreply.github.com>
2025-11-19 16:25:09 -0800

    Implements database header

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 
 #include "common.h"
 #include "file.h"
-
+#include "parse.h"
 
 void print_usage(char *argv[]) {
     printf("Usage: %s -n -f <database file>\n", argv[0]);
@@ -21,6 +21,7 @@ int main(int argc, char *argv[]) {
     int c;
 
     int dbfd = -1;
+    struct dbheader_t *dbhdr = NULL;
 
     while((c = getopt(argc, argv, "nf: ")) != -1) {
         switch (c) {
@@ -52,12 +53,23 @@ int main(int argc, char *argv[]) {
             printf("Unable to create database file\n");
             return -1;
         }
+
+        if (create_db_header(dbfd, &dbhdr) == STATUS_ERROR) {
+            printf("Failed to create database header\n");
+            return STATUS_ERROR;
+        }
     } else {
         dbfd = open_db_file(filepath);
         if (dbfd == STATUS_ERROR) {
             printf("Unable to open database file\n");
             return -1;
         }
+        
+        if (validate_db_header(dbfd, &dbhdr) == STATUS_ERROR) {
+            printf("Failed to validate database header\n");
+            return -1;
+        }
+
     }
 
 
---
+++ src/parse.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "common.h"
+#include "parse.h"
+
+int validate_db_header(int fd, struct dbheader_t **headerOut) {
+    if (fd < 0) {
+        printf("Got a bad FD from the user\n");
+        return STATUS_ERROR;
+    }
+
+    struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
+    if (header == -1) {
+        printf("Mallow failed to create a db header\n");
+        return STATUS_ERROR;
+    }
+
+    if (read(fd, header, sizeof(struct dbheader_t)) != sizeof(struct dbheader_t)) {
+        perror("read");
+        free(header);
+        return STATUS_ERROR;
+    }
+
+    header->version = ntohs(header->version);
+    header->count = ntohs(header->count);
+    header->magic = ntohl(header->magic);
+    header->filesize = ntohl(header->filesize);
+
+    if (header->magic != HEADER_MAGIC) {
+        printf("Impromper header magic\n");
+        free(header);
+        return -1;
+    }
+
+
+    if (header->version != 1) {
+        printf("Impromper header version\n");
+        free(header);
+        return -1;
+    }
+
+
+    struct stat dbstat = {0};
+    fstat(fd, &dbstat);
+    if (header->filesize != dbstat.st_size) {
+        printf("Corrupted database\n");
+        free(header);
+        return -1; 
+    }
+
+}
+
+
+int create_db_header(int fd, struct dbheader_t **headerOut) {
+    struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
+    if (header == -1) {
+        printf("Malloc failed to create db header\n");
+        return STATUS_ERROR;
+        
+    }
+    header->version = 0x1;
+    header->count = 0;
+    header->magic = HEADER_MAGIC;
+    header->filesize = sizeof(struct dbheader_t);
+
+    *headerOut = header;
+
+    return STATUS_SUCCESS;
+}
